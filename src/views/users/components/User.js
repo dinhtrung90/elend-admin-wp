7613c3d665b6d630b171eaca05cb438d4103e04d
nhutphan <minhnhut0602@gmail.com>
2021-06-04 17:34:23 +0700

    working on user create form

--- src/views/users/components/User.js
+++ src/views/users/components/User.js
@@ -13,6 +13,9 @@ import {
 import CIcon from '@coreui/icons-react'
 import {userActions} from "../actions";
 import {useTranslation} from "react-i18next";
+import { useFormik } from "formik";
+import * as Yup from "yup";
+import 'yup-phone';
 
 const User = ({match}) => {
   const { t } = useTranslation();
@@ -21,6 +24,36 @@ const User = ({match}) => {
 
   const userDetails = user ? Object.entries(user) : 
     [['id', (<span><CIcon className="text-muted" name="cui-icon-ban" /> Not found</span>)]]
+
+  const permissions = ['ROLE_ADMIN', 'ROLE_USER'];
+
+  const formik = useFormik({
+    initialValues: {
+      username: '',
+      userRole: '',
+      email: '',
+      firstName: '',
+      lastName: '',
+      mobilePhone: '',
+      birthDate: '',
+      addressLine1: '',
+      addressLine2: '',
+      city: '',
+      country: '',
+      zipCode: ''
+    },
+    validationSchema: Yup.object({
+      username: Yup.string().required(),
+      userRole: Yup.string().required().oneOf(permissions),
+      email: Yup.string().email().required(),
+      firstName: Yup.string().email().required(),
+      lastName: Yup.string().email().required(),
+      mobilePhone: Yup.string().phone('VN').required()
+    }),
+    onSubmit: values => {
+      console.log('onSubmit values=', values);
+    }
+  });
 
   useEffect(() => {
     dispatch(userActions.getUserDetail(match.params.id));
@@ -43,7 +76,13 @@ const User = ({match}) => {
                         <CIcon name="cil-user" />
                       </CInputGroupText>
                     </CInputGroupPrepend>
-                    <CInput id="username" name="username" placeholder={t('view.User.Username')} />
+                    <CInput invalid={formik.errors.username && formik.touched.username}
+                      id="username"
+                      name="username"
+                      value={formik.values.username}
+                      placeholder={t('view.User.Username')}
+                      {...formik.getFieldProps("username")}
+                    />
                   </CInputGroup>
                 </CCol>
                 <CCol sm={3} className="mb-4">
@@ -54,10 +93,11 @@ const User = ({match}) => {
                         <CIcon name="cil-people" />
                       </CInputGroupText>
                     </CInputGroupPrepend>
-                    <CSelect custom name="selectUserRole" id="selectUserRole">
+                    <CSelect custom name="selectUserRole" id="selectUserRole" invalid={formik.errors.userRole && formik.touched.userRole}
+                       {...formik.getFieldProps("userRole")}>
                       <option value="0">{t('messages.messagePleaseSelect')}</option>
-                      <option value="1">ROLE_ADMIN</option>
-                      <option value="2">ROLE_USER</option>
+                      <option value="ROLE_ADMIN">ROLE_ADMIN</option>
+                      <option value="ROLE_USER">ROLE_USER</option>
                     </CSelect>
                   </CInputGroup>
                 </CCol>
@@ -69,7 +109,12 @@ const User = ({match}) => {
                         <CIcon name="cil-envelope-closed" />
                       </CInputGroupText>
                     </CInputGroupPrepend>
-                    <CInput id="EmailAddress" name="EmailAddress" placeholder={t('view.User.EmailAddress')} />
+                    <CInput id="EmailAddress" name="EmailAddress"
+                      invalid={formik.errors.email && formik.touched.email}
+                      placeholder={t('view.User.EmailAddress')}
+                      value={formik.values.email}
+                      {...formik.getFieldProps("email")}
+                    />
                   </CInputGroup>
                 </CCol>
                 <CCol sm={3} className="mb-4">
@@ -80,7 +125,11 @@ const User = ({match}) => {
                         <CIcon name="cil-user" />
                       </CInputGroupText>
                     </CInputGroupPrepend>
-                    <CInput id="FirstName" name="FirstName" placeholder={t('view.User.FirstName')} />
+                    <CInput id="FirstName" name="FirstName" placeholder={t('view.User.FirstName')}
+                      invalid={formik.errors.firstName && formik.touched.firstName}
+                      value={formik.values.firstName}
+                      {...formik.getFieldProps("firstName")}
+                    />
                   </CInputGroup>
                 </CCol>
                 <CCol sm={3} className="mb-4">
@@ -91,7 +140,11 @@ const User = ({match}) => {
                         <CIcon name="cil-user" />
                       </CInputGroupText>
                     </CInputGroupPrepend>
-                    <CInput id="LastName" name="LastName" placeholder={t('view.User.LastName')} />
+                    <CInput id="LastName" name="LastName" placeholder={t('view.User.LastName')}
+                      invalid={formik.errors.lastName && formik.touched.lastName}
+                      value={formik.values.lastName}
+                      {...formik.getFieldProps("lastName")}
+                    />
                   </CInputGroup>
                 </CCol>
                 <CCol sm={6} className="mb-4">
@@ -102,7 +155,11 @@ const User = ({match}) => {
                         <CIcon name="cil-phone" />
                       </CInputGroupText>
                     </CInputGroupPrepend>
-                    <CInput type="number" id="MobileNumber" name="MobileNumber" placeholder={t('common.MobileNumber')} />
+                    <CInput type="number" id="MobileNumber" name="MobileNumber" placeholder={t('common.MobileNumber')}
+                      invalid={formik.errors.mobilePhone && formik.touched.mobilePhone}
+                      value={formik.values.mobilePhone}
+                      {...formik.getFieldProps("mobilePhone")}
+                    />
                   </CInputGroup>
                 </CCol>
                 <CCol sm={6} className="mb-4">
@@ -113,7 +170,7 @@ const User = ({match}) => {
                         <CIcon name="cil-calendar" />
                       </CInputGroupText>
                     </CInputGroupPrepend>
-                    <CInput type="date" id="DateOfBirth" name="DateOfBirth" />
+                    <CInput type="date" id="DateOfBirth" name="DateOfBirth" value={formik.values.birthDate}/>
                   </CInputGroup>
                 </CCol>
                 <CCol sm={6} className="mb-4">
@@ -142,7 +199,7 @@ const User = ({match}) => {
                         <CIcon name="cil-location-pin" />
                       </CInputGroupText>
                     </CInputGroupPrepend>
-                    <CInput id="AddressLine1" name="AddressLine1" placeholder={t('view.User.AddressLine1')} />
+                    <CInput id="AddressLine1" name="AddressLine1" placeholder={t('view.User.AddressLine1')} value={formik.values.addressLine1} />
                   </CInputGroup>
                 </CCol>
                 <CCol sm={6} className="mb-4">
@@ -153,7 +210,7 @@ const User = ({match}) => {
                         <CIcon name="cil-location-pin" />
                       </CInputGroupText>
                     </CInputGroupPrepend>
-                    <CInput id="AddressLine2" name="AddressLine2" placeholder={t('view.User.AddressLine2')} />
+                    <CInput id="AddressLine2" name="AddressLine2" placeholder={t('view.User.AddressLine2')} value={formik.values.addressLine2} />
                   </CInputGroup>
                 </CCol>
                 <CCol sm={3} className="mb-4">
@@ -164,7 +221,7 @@ const User = ({match}) => {
                         <CIcon name="cil-location-pin" />
                       </CInputGroupText>
                     </CInputGroupPrepend>
-                    <CInput id="AreaOrCity" name="AreaOrCity" placeholder={t('view.User.AreaOrCity')} />
+                    <CInput id="AreaOrCity" name="AreaOrCity" placeholder={t('view.User.AreaOrCity')} value={formik.values.city} />
                   </CInputGroup>
                 </CCol>
                 <CCol sm={3} className="mb-4">
@@ -175,7 +232,7 @@ const User = ({match}) => {
                         <CIcon name="cil-map" />
                       </CInputGroupText>
                     </CInputGroupPrepend>
-                    <CInput id="Country" name="Country" placeholder={t('view.User.Country')} />
+                    <CInput id="Country" name="Country" placeholder={t('view.User.Country')} value={formik.values.country}/>
                   </CInputGroup>
                 </CCol>
                 <CCol sm={6} className="mb-4">
@@ -186,7 +243,7 @@ const User = ({match}) => {
                         <CIcon name="cil-location-pin" />
                       </CInputGroupText>
                     </CInputGroupPrepend>
-                    <CInput id="PostalZipCode" name="PostalZipCode" placeholder={t('view.User.PostalZipCode')} />
+                    <CInput id="PostalZipCode" name="PostalZipCode" placeholder={t('view.User.PostalZipCode')} value={formik.values.zipCode} />
                   </CInputGroup>
                 </CCol>
               </CRow>
