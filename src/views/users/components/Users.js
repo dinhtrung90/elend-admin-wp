7d26c53017b81fb53d574c98e59c05d270f008cf
nhutphan <nhut.phan@adurolife.com>
2021-06-25 08:50:46 +0700

    update users ui

--- src/views/users/components/Users.js
+++ src/views/users/components/Users.js
@@ -139,7 +139,7 @@ const Users = () => {
                           </td>
                       ),
                   'emailVerified': (item) => (
-                      <td style={{width: '120px', 'text-align': 'center'}}>
+                      <td style={{width: '120px', 'textAlign': 'center'}}>
                         {item.verifiedEmail ? <FaRegCheckSquare size="1.5em" /> : <FaRegSquare size="1.5em" />}
                       </td>
                   ),
