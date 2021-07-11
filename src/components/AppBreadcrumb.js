f29f4f6061292eb94af8c803a7bbbfe254f2ab32
nhutphan <nhut.phan@adurolife.com>
2021-07-11 21:42:17 +0700

    fixed breadcumb

--- src/components/AppBreadcrumb.js
+++ src/components/AppBreadcrumb.js
@@ -9,26 +9,31 @@ const AppBreadcrumb = () => {
   const currentLocation = useLocation().pathname
 
   const getRouteName = (pathname, routes) => {
+    console.log('getRouteName=', pathname)
     const currentRoute = routes.find((route) => route.path === pathname)
     return currentRoute ? currentRoute.name : ''
   }
 
   const getBreadcrumbs = (location) => {
     const breadcrumbs = []
+    console.log('location=', location)
     location.split('/').reduce((prev, curr, index, array) => {
       const currentPathname = `${prev}/${curr}`
-      breadcrumbs.push({
-        pathname: currentPathname,
-        name: getRouteName(currentPathname, routes),
-        active: index + 1 === array.length ? true : false,
-      })
+      const routeName = getRouteName(currentPathname, routes)
+      if (routeName && routeName.length > 0) {
+        breadcrumbs.push({
+          pathname: `#${currentPathname}`,
+          name: routeName,
+          active: index + 1 === array.length ? true : false,
+        })
+      }
       return currentPathname
     })
     return breadcrumbs
   }
 
   const breadcrumbs = getBreadcrumbs(currentLocation)
-
+  console.log('breadcrumbs=', breadcrumbs)
   return (
     <CBreadcrumb className="m-0 ms-2">
       <CBreadcrumbItem href="/">Home</CBreadcrumbItem>
