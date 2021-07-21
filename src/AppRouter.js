9687d620597b661b32d2eb0d3530d4bbd83e14a4
nhutphan <nhut.phan@adurolife.com>
2021-07-21 22:30:41 +0700

    added register page

--- src/AppRouter.js
+++ src/AppRouter.js
@@ -5,7 +5,7 @@ import { useKeycloak } from '@react-keycloak/web'
 
 const DefaultLayout = React.lazy(() => import('./layout/DefaultLayout'))
 const Login = React.lazy(() => import('./views/pages/login/Login'))
-
+const Register = React.lazy(() => import('./views/pages/register/Register'))
 const Page403 = React.lazy(() => import('./views/pages/page403/Page403'))
 
 export const AppRouter = () => {
@@ -17,6 +17,7 @@ export const AppRouter = () => {
 
   return (
     <Switch>
+      <Route exact path="/register" render={() => <Register />} />
       <Route exact path="/login" render={() => <Login />} />
       <Route exact path="/403" render={() => <Page403 />} />
       <PrivateRoute path="/" name="Home" component={DefaultLayout} />
--- src/routes.js
+++ src/routes.js
@@ -46,7 +46,7 @@ const Modals = React.lazy(() => import('./views/components/notifications/modals/
 const Toasts = React.lazy(() => import('./views/components/notifications/toasts/Toasts'))
 
 // const Login = React.lazy(() => import('./views/examples/pages/login/Login'))
-// const Register = React.lazy(() => import('./views/examples/pages/register/Register'))
+const Register = React.lazy(() => import('./views/pages/register/Register'))
 // const Page404 = React.lazy(() => import('./views/examples/pages/page404/Page404'))
 // const Page500 = React.lazy(() => import('./views/examples/pages/page500/Page500'))
 
@@ -113,7 +113,7 @@ const routes = [
   { path: '/notifications/modals', name: 'Modals', component: Modals },
   { path: '/notifications/toasts', name: 'Toasts', component: Toasts },
   // { path: '/login', name: 'Login', component: Login },
-  // { path: '/register', name: 'Register', component: Register },
+  { path: '/register', name: 'Register', component: Register },
   // { path: '/404', name: '404', component: Page404 },
   // { path: '/500', name: '500', component: Page500 },
   { path: '/widgets', name: 'Widgets', component: Widgets },
