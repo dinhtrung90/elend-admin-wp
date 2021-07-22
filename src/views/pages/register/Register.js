a44705ba2b7e9dcffa138d3d7246a629ae8bf25a
nhutphan <nhut.phan@adurolife.com>
2021-07-22 18:13:35 +0700

    validate reward form

--- src/views/pages/register/Register.js
+++ src/views/pages/register/Register.js
@@ -13,6 +13,7 @@ import {
   CInputGroupText,
   CRow,
   CFormSelect,
+  CFormFeedback,
 } from '@coreui/react'
 import CIcon from '@coreui/icons-react'
 import { FaAddressCard } from 'react-icons/fa'
@@ -20,6 +21,7 @@ import * as Yup from 'yup'
 
 const Register = () => {
   const constGenders = {
+    NONE: 'NONE',
     MALE: 'Male',
     FEMALE: 'Female',
     UNKNOWN: 'Unknown',
@@ -29,6 +31,8 @@ const Register = () => {
     mobilePhone: Yup.string().required('Vui lòng nhập số điện thoại'),
     ssn: Yup.string().required('Vui lòng nhập CMND hay CCCD'),
     fullAddress: Yup.string().required('Vui lòng địa chỉ'),
+    fileBeforeCard: Yup.mixed().required('Vui lòng chụp mặt trước CMND/CCCD'),
+    fileAfterCard: Yup.mixed().required('Vui lòng chụp mặt sau CMND/CCCD'),
   })
 
   const formik = useFormik({
@@ -36,15 +40,20 @@ const Register = () => {
       fullName: '',
       mobilePhone: '',
       ssn: '', // CMND or CCCD
-      fileBeforeCard: null,
-      fileAfterCard: null,
+      fileBeforeCard: '',
+      fileAfterCard: '',
       gender: '',
       birthDate: '',
       fullAddress: '',
     },
     enableReinitialize: true,
     validationSchema: schema,
+    onSubmit: (values) => handleToSubmitAccount(values),
   })
+
+  const handleToSubmitAccount = (values) => {
+    console.logs('submit=', values)
+  }
 
   return (
     <div className="bg-light min-vh-100 d-flex flex-row align-items-center">
@@ -53,7 +62,7 @@ const Register = () => {
           <CCol md="9" lg="7" xl="6">
             <CCard className="mx-4">
               <CCardBody className="p-4">
-                <CForm>
+                <CForm onSubmit={formik.handleSubmit}>
                   <h1>Quà tặng</h1>
                   <p className="text-medium-emphasis">Đăng ký nhận quà, tránh xa Covid </p>
                   <CRow>
@@ -70,8 +79,19 @@ const Register = () => {
                           name="fullName"
                           placeholder="Họ và tên"
                           value={formik.values.fullName}
+                          invalid={formik.errors.fullName && formik.touched.fullName}
+                          {...formik.getFieldProps('fullName')}
                         />
                       </CInputGroup>
+                      <CFormFeedback
+                        invalid
+                        style={{
+                          display:
+                            formik.errors.fullName && formik.touched.fullName ? 'block' : 'none',
+                        }}
+                      >
+                        {formik.errors.fullName}
+                      </CFormFeedback>
                     </CCol>
                     <CCol sm={12} className="mb-4">
                       <CFormLabel htmlFor="MobileNumber" className="col-form-label">
@@ -86,9 +106,22 @@ const Register = () => {
                           id="MobileNumber"
                           name="MobileNumber"
                           placeholder="Số điện thoại"
+                          invalid={formik.errors.mobilePhone && formik.touched.mobilePhone}
                           value={formik.values.mobilePhone}
+                          {...formik.getFieldProps('mobilePhone')}
                         />
                       </CInputGroup>
+                      <CFormFeedback
+                        invalid
+                        style={{
+                          display:
+                            formik.errors.mobilePhone && formik.touched.mobilePhone
+                              ? 'block'
+                              : 'none',
+                        }}
+                      >
+                        {formik.errors.mobilePhone}
+                      </CFormFeedback>
                     </CCol>
                     <CCol sm={12} className="mb-4">
                       <CFormLabel htmlFor="ssn" className="col-form-label">
@@ -102,9 +135,19 @@ const Register = () => {
                           id="ssn"
                           name="ssn"
                           placeholder="CMND / CCCD"
+                          invalid={formik.errors.ssn && formik.touched.ssn}
                           value={formik.values.ssn}
+                          {...formik.getFieldProps('ssn')}
                         />
                       </CInputGroup>
+                      <CFormFeedback
+                        invalid
+                        style={{
+                          display: formik.errors.ssn && formik.touched.ssn ? 'block' : 'none',
+                        }}
+                      >
+                        {formik.errors.ssn}
+                      </CFormFeedback>
                     </CCol>
                     <CCol sm={12} className="mb-4">
                       <CFormLabel htmlFor="beforeIdentityCard" className="col-form-label">
@@ -118,9 +161,22 @@ const Register = () => {
                           id="beforeIdentityCard"
                           type="file"
                           name="beforeIdentityCard"
+                          invalid={formik.errors.fileBeforeCard && formik.touched.fileBeforeCard}
                           value={formik.values.fileBeforeCard}
+                          {...formik.getFieldProps('fileBeforeCard')}
                         />
                       </CInputGroup>
+                      <CFormFeedback
+                        invalid
+                        style={{
+                          display:
+                            formik.errors.fileBeforeCard && formik.touched.fileBeforeCard
+                              ? 'block'
+                              : 'none',
+                        }}
+                      >
+                        {formik.errors.fileBeforeCard}
+                      </CFormFeedback>
                     </CCol>
                     <CCol sm={12} className="mb-4">
                       <CFormLabel htmlFor="afterIdentityCard" className="col-form-label">
@@ -134,9 +190,22 @@ const Register = () => {
                           id="afterIdentityCard"
                           type="file"
                           name="afterIdentityCard"
+                          invalid={formik.errors.fileAfterCard && formik.touched.fileAfterCard}
                           value={formik.values.fileAfterCard}
+                          {...formik.getFieldProps('fileAfterCard')}
                         />
                       </CInputGroup>
+                      <CFormFeedback
+                        invalid
+                        style={{
+                          display:
+                            formik.errors.fileAfterCard && formik.touched.fileAfterCard
+                              ? 'block'
+                              : 'none',
+                        }}
+                      >
+                        {formik.errors.fileAfterCard}
+                      </CFormFeedback>
                     </CCol>
                     <CCol sm={12} className="mb-4">
                       <CFormLabel htmlFor="UserRole" className="col-form-label">
@@ -146,8 +215,8 @@ const Register = () => {
                         <CInputGroupText>
                           <CIcon name="cil-people" />
                         </CInputGroupText>
-                        <CFormSelect custom name="Gender" id="Gender">
-                          <option value="0">Chọn giới tính</option>
+                        <CFormSelect name="Gender" id="Gender">
+                          <option value={constGenders.NONE}>Chọn giới tính</option>
                           <option value={constGenders.MALE}>Nam</option>
                           <option value={constGenders.FEMALE}>Nữ</option>
                           <option value={constGenders.UNKNOWN}>Khác</option>
@@ -177,11 +246,25 @@ const Register = () => {
                           id="fullAddress"
                           name="fullAddress"
                           placeholder="Địa chỉ nhà"
+                          invalid={formik.errors.fullAddress && formik.touched.fullAddress}
+                          value={formik.values.fullAddress}
+                          {...formik.getFieldProps('fullAddress')}
                         />
                       </CInputGroup>
+                      <CFormFeedback
+                        invalid
+                        style={{
+                          display:
+                            formik.errors.fullAddress && formik.touched.fullAddress
+                              ? 'block'
+                              : 'none',
+                        }}
+                      >
+                        {formik.errors.fullAddress}
+                      </CFormFeedback>
                     </CCol>
                   </CRow>
-                  <CButton color="success" block>
+                  <CButton type="submit" color="success">
                     Đăng ký
                   </CButton>
                 </CForm>
