27bbb582a7ad9a6d0a8f2983976e2dceff99f153
nhutphan <nhut.phan@adurolife.com>
2021-07-25 18:37:09 +0700

    update reward content

--- src/views/pages/register/Register.js
+++ src/views/pages/register/Register.js
@@ -263,7 +263,31 @@ const Register = () => {
       ></div>
       <CContainer>
         <CRow className="justify-content-center">
-          <CCol md="9" lg="9" xl="8">
+          <CCol sm={12} className="mt-4 mb-4">
+            <div className="reward-description-content mx-4">
+              <p>
+                Cùng Chung Tay đánh bay covid Công ty cổ phần phần TND Technology Solution hỗ trợ{' '}
+                <strong>1000 phần quà</strong> chia sẻ khó khăn cùng anh em lái xe công nghệ vượt
+                qua đợt dịch covid này
+              </p>
+              <p>Cách thức nhận quà:</p>
+              <p>
+                <strong>Bước 1:</strong> Cung cấp thông tin theo đường link bên dưới Sau khi cung
+                cấp thông tin sẽ có nhân viên liên lạc xác nhận ngày các bạn sẽ nhận quà
+              </p>
+              <p>
+                <strong>Bước 2:</strong> Để đảm bảo an toàn phòng dịch khi tới nhận quà các bạn chỉ
+                cần Mở Zalo và quét mã QR tại điểm nhận quà.
+              </p>
+              <p>Nhân viên tại điểm nhận quà sẽ kiểm tra đúng thông tin</p>
+              <p>Anh/Chị tự lấy quà tại điểm trao quà (không tiếp xúc)</p>
+              <p>
+                Dự kiến ngày <strong>28-29-30/7</strong> (mỗi ngày 300 phần quà) tại địa chỉ 198
+                Nguyễn Văn Luông Phường 11 Quận 6
+              </p>
+            </div>
+          </CCol>
+          <CCol sm={12}>
             <CCard className="mx-4">
               <CCardBody className="p-4">
                 <CForm onSubmit={formik.handleSubmit}>
